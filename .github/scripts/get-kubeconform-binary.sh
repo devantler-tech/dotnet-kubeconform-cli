c0840c29242e1b3f06ef74bdf6106f9a8cc68d1b
Nikolai Emil Damm <ned@devantler.com>
2024-07-08 15:21:55 +0200

    feat: Add script to download Kubeconform binaries

---
+++ .github/scripts/get-kubeconform-binary.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+get() {
+  local url=$1
+  local binary=$2
+  local target_dir=$3
+  local target_name=$4
+  local isTar=$5
+
+  # check if tar
+  if [ "$isTar" = true ]; then
+    curl -LJ "$url" | tar xvz -C "$target_dir" "$binary"
+    mv "$target_dir/$binary" "${target_dir}/$target_name"
+  elif [ "$isTar" = false ]; then
+    curl -LJ "$url" -o "$target_dir/$target_name"
+  fi
+  chmod +x "$target_dir/$target_name"
+}
+
+get "https://getbin.io/yannh/kubeconform?os=darwin&arch=amd64" "kubeconform" "src/KSail/assets/binaries" "kubeconform-darwin-amd64" true
+get "https://getbin.io/yannh/kubeconform?os=darwin&arch=arm64" "kubeconform" "src/KSail/assets/binaries" "kubeconform-darwin-arm64" true
+get "https://getbin.io/yannh/kubeconform?os=linux&arch=amd64" "kubeconform" "src/KSail/assets/binaries" "kubeconform-linux-amd64" true
+get "https://getbin.io/yannh/kubeconform?os=linux&arch=arm64" "kubeconform" "src/KSail/assets/binaries" "kubeconform-linux-arm64" true
