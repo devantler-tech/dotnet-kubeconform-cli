da11f7c029bd112e43273eae0338d73bc49e3855
Nikolai Emil Damm <ned@devantler.com>
2024-10-31 21:05:48 +0100

    Add KubeconformCLI (#6)
    * Add KubeconformCLI
    
    * Refactor RunAsync method to improve parameter handling and add default values; enhance tests for default execution

---
+++ Devantler.KubeconformCLI/Kubeconform.cs
@@ -0,0 +1,42 @@
+using System.Runtime.InteropServices;
+using CliWrap;
+using Devantler.CLIRunner;
+
+namespace Devantler.KubeconformCLI;
+
+class Kubeconform()
+{
+  static Command Command => GetCommand();
+  internal static Command GetCommand(PlatformID? platformID = default, Architecture? architecture = default, string? runtimeIdentifier = default)
+  {
+    platformID ??= Environment.OSVersion.Platform;
+    architecture ??= RuntimeInformation.ProcessArchitecture;
+    runtimeIdentifier ??= RuntimeInformation.RuntimeIdentifier;
+
+    string binary = (platformID, architecture, runtimeIdentifier) switch
+    {
+      (PlatformID.Unix, Architecture.X64, "osx-x64") => "kubeconform-osx-x64",
+      (PlatformID.Unix, Architecture.Arm64, "osx-arm64") => "kubeconform-osx-arm64",
+      (PlatformID.Unix, Architecture.X64, "linux-x64") => "kubeconform-linux-x64",
+      (PlatformID.Unix, Architecture.Arm64, "linux-arm64") => "kubeconform-linux-arm64",
+      (PlatformID.Win32NT, Architecture.X64, "win-x64") => "kubeconform-win-x64.exe",
+      (PlatformID.Win32NT, Architecture.X64, "win-arm64") => "kubeconform-win-arm64.exe",
+      _ => throw new PlatformNotSupportedException($"Unsupported platform: {Environment.OSVersion.Platform} {RuntimeInformation.ProcessArchitecture}"),
+    };
+    string binaryPath = Path.Combine(AppContext.BaseDirectory, binary);
+    return !File.Exists(binaryPath) ?
+      throw new FileNotFoundException($"{binaryPath} not found.") :
+      Cli.Wrap(binaryPath);
+  }
+
+  internal static async Task RunAsync(string file, string[]? kubeconformFlags = null, string[]? kubeconformConfig = null, CancellationToken cancellationToken = default)
+  {
+    kubeconformFlags ??= [];
+    kubeconformConfig ??= ["-strict", "-ignore-missing-schemas", "-schema-location", "default", "-schema-location", "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json", "-verbose"];
+    var arguments = kubeconformFlags.Concat(kubeconformConfig).Concat([file]);
+    var cmd = Command.WithArguments(arguments);
+    var (exitCode, result) = await CLI.RunAsync(cmd, silent: true, cancellationToken: cancellationToken).ConfigureAwait(false);
+    if (exitCode != 0)
+      throw new KubeconformException($"Kubeconform failed with exit code {exitCode} and error: {result}");
+  }
+}
--- README.md
+++ README.md
@@ -1,16 +1,65 @@
 # 🔎 .NET Kubeconform CLI
 
 [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Test](https://github.com/devantler/dotnet-kubeconform-cli/actions/workflows/test.yaml/badge.svg)](https://github.com/devantler/dotnet-kubeconform-cli/actions/workflows/test.yaml)
+[![codecov](https://codecov.io/gh/devantler/dotnet-kubeconform-cli/graph/badge.svg?token=RhQPb4fE7z)](https://codecov.io/gh/devantler/dotnet-kubeconform-cli)
+
+<details>
+  <summary>Show/hide folder structure</summary>
 
 <!-- readme-tree start -->
+
 ```
 .
 ├── .github
 │   ├── scripts
 │   └── workflows
-├── src
-└── tests
+├── Devantler.KindCLI
+│   └── runtimes
+│       ├── linux-arm64
+│       │   └── native
+│       ├── linux-x64
+│       │   └── native
+│       ├── osx-arm64
+│       │   └── native
+│       ├── osx-x64
+│       │   └── native
+│       └── win-x64
+│           └── native
+└── Devantler.KindCLI.Tests
+    ├── KindTests
+    └── assets
 
-5 directories
+18 directories
 ```
+
 <!-- readme-tree end -->
+
+</details>
+
+A simple .NET library that embeds the Kubeconform CLI.
+
+## 🚀 Getting Started
+
+To get started, you can install the package from NuGet.
+
+```bash
+dotnet add package Devantler.KubeconformCLI
+```
+
+## 📝 Usage
+
+You can execute the Kubeconform CLI commands using the `Kubeconform` class.
+
+```csharp
+using Devantler.KubeconformCLI;
+
+string[] kubeconformFlags = ["-skip=Secret"];
+string[] kubeconformConfig = ["-strict", "-ignore-missing-schemas", "-schema-location", "default", "-schema-location", "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json", "-verbose"];
+string [] files = Directory.GetFiles("path/to/manifests", "*.yaml", SearchOption.AllDirectories);
+
+foreach (string file in files)
+{
+    _ = await Kubeconform.ValidateAsync(file, kubeconformFlags, kubeconformConfig, cancellationToken);
+}
+```
