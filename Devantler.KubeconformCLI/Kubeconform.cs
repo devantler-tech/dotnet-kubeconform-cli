488575d0921059f3a647ce6c399de350a1300505
Nikolai Emil Damm <ned@devantler.com>
2025-02-26 13:28:31 +0100

    fix: correct architecture mapping for Windows ARM64 in GetCommand (#71)
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- Devantler.KubeconformCLI/Kubeconform.cs
+++ Devantler.KubeconformCLI/Kubeconform.cs
@@ -26,7 +26,7 @@ public static class Kubeconform
       (PlatformID.Unix, Architecture.X64, "linux-x64") => "kubeconform-linux-x64",
       (PlatformID.Unix, Architecture.Arm64, "linux-arm64") => "kubeconform-linux-arm64",
       (PlatformID.Win32NT, Architecture.X64, "win-x64") => "kubeconform-win-x64.exe",
-      (PlatformID.Win32NT, Architecture.X64, "win-arm64") => "kubeconform-win-arm64.exe",
+      (PlatformID.Win32NT, Architecture.Arm64, "win-arm64") => "kubeconform-win-arm64.exe",
       _ => throw new PlatformNotSupportedException($"Unsupported platform: {Environment.OSVersion.Platform} {RuntimeInformation.ProcessArchitecture}"),
     };
     string binaryPath = Path.Combine(AppContext.BaseDirectory, binary);
