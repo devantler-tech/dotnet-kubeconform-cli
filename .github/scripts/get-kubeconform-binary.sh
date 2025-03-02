87e2b60f88a51afe439856901892c877b7fa5707
Nikolai Emil Damm <neq@energinet.dk>
2025-03-02 01:07:14 +0100

    chore: update paths to kubeconform binaries in get-kubeconform-binary.sh
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- .github/scripts/get-kubeconform-binary.sh
+++ .github/scripts/get-kubeconform-binary.sh
@@ -23,10 +23,10 @@ get() {
   chmod +x "$target_dir/$target_name"
 }
 
-get "https://getbin.io/yannh/kubeconform?os=darwin&arch=arm64" "kubeconform" "Devantler.KubeconformCLI/runtimes/osx-x64/native" "kubeconform-osx-x64" "tar"
-get "https://getbin.io/yannh/kubeconform?os=darwin&arch=amd64" "kubeconform" "Devantler.KubeconformCLI/runtimes/osx-arm64/native" "kubeconform-osx-arm64" "tar"
-get "https://getbin.io/yannh/kubeconform?os=linux&arch=arm64" "kubeconform" "Devantler.KubeconformCLI/runtimes/linux-arm64/native" "kubeconform-linux-arm64" "tar"
-get "https://getbin.io/yannh/kubeconform?os=linux&arch=amd64" "kubeconform" "Devantler.KubeconformCLI/runtimes/linux-x64/native" "kubeconform-linux-x64" "tar"
-get "https://getbin.io/yannh/kubeconform?os=windows&arch=amd64" "kubeconform.exe" "Devantler.KubeconformCLI/runtimes/win-x64/native" "kubeconform-win-x64.exe" "zip"
-get "https://getbin.io/yannh/kubeconform?os=windows&arch=arm64" "kubeconform.exe" "Devantler.KubeconformCLI/runtimes/win-arm64/native" "kubeconform-win-arm64.exe" "zip"
+get "https://getbin.io/yannh/kubeconform?os=darwin&arch=arm64" "kubeconform" "src/Devantler.KubeconformCLI/runtimes/osx-x64/native" "kubeconform-osx-x64" "tar"
+get "https://getbin.io/yannh/kubeconform?os=darwin&arch=amd64" "kubeconform" "src/Devantler.KubeconformCLI/runtimes/osx-arm64/native" "kubeconform-osx-arm64" "tar"
+get "https://getbin.io/yannh/kubeconform?os=linux&arch=arm64" "kubeconform" "src/Devantler.KubeconformCLI/runtimes/linux-arm64/native" "kubeconform-linux-arm64" "tar"
+get "https://getbin.io/yannh/kubeconform?os=linux&arch=amd64" "kubeconform" "src/Devantler.KubeconformCLI/runtimes/linux-x64/native" "kubeconform-linux-x64" "tar"
+get "https://getbin.io/yannh/kubeconform?os=windows&arch=amd64" "kubeconform.exe" "src/Devantler.KubeconformCLI/runtimes/win-x64/native" "kubeconform-win-x64.exe" "zip"
+get "https://getbin.io/yannh/kubeconform?os=windows&arch=arm64" "kubeconform.exe" "src/Devantler.KubeconformCLI/runtimes/win-arm64/native" "kubeconform-win-arm64.exe" "zip"
 rm -rf Devantler.KubeconformCLI/runtimes/*/native/LICENSE
